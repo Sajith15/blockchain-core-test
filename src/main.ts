6adf286fdc0dcca56e789904a93aeff6eb02f951
Sajith15 <kpsajith2000@gmail.com>
2022-12-11 20:46:50 +0530

    add main.ts with send transaction function

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { pushTxtoMemPool } from "./mempool";
+import {mainExec} from "./executeTxn";
+
+interface data {
+  addModule: string;
+  from: string;
+  values: object;
+}
+
+interface values {
+  data: data;
+  time: string;
+}
+
+const sendTransactionParams = (val: values, sentTime: string) => {
+  let incomingTx: object = val;
+  val.time = sentTime;
+  pushTxtoMemPool(incomingTx);
+  myFunc();
+};
+
+let myFunc = () => {
+  myFunc = function () {}; // kill it as soon as it was called
+  mainExec();
+};
+
+export { sendTransactionParams };
